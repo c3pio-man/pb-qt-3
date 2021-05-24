028bbe8fd663b21af3b45fc4a4161c52091d5b64
c3pio-man <50335692+c3pio-man@users.noreply.github.com>
2021-05-24 13:51:48 +0300

    SDK, used for build 632.6.1.900

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Download SDK v 6.1.."
+echo "Download SDK v 6.1.upd0."
 mkdir -p SDK-6.3.0
 cd SDK-6.3.0
-wget -O SDK-B288-6.1.7z  https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.1/SDK-B288-6.1.7z && 7z x -aoa -y SDK-B288-6.1.7z
+wget -O SDK-B288-6.1.upd0.7z  https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.1.0/SDK-B288-6.1.upd0.7z && 7z x -aoa -y SDK-B288-6.1.upd0.7z
