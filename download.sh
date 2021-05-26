6f08ab0bac0a7f5ee987d8b56d728e07a2c7c0aa
c3pio-man <50335692+c3pio-man@users.noreply.github.com>
2021-05-26 21:06:42 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,4 +3,4 @@
 echo "Download SDK v 6.3.upd1"
 mkdir -p SDK-6.3.0
 cd SDK-6.3.0
-wget -O SDK-B288-6.3.7z  https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.3.1/SDK-B288-6.3.upd1.7z && 7z x -aoa -y SDK-B288-6.3.7z
+wget -O SDK-B288-6.3.upd2.7z https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.3.2/SDK-B288-6.3.upd2.7z && 7z x -aoa -y SDK-B288-6.3.upd2.7z
