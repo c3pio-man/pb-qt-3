c7f2b6692504fedf3bf2c38d84b4644fb0fdde70
c3pio-man <50335692+c3pio-man@users.noreply.github.com>
2022-09-20 13:24:54 +0300

    #140514 - upd path in download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Download SDK v 6.3.upd2"
+echo "Download SDK v 6.7"
 mkdir -p SDK_6.3.0
 cd SDK_6.3.0
-wget -O SDK-B288-6.3.upd2.7z https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.3.2/SDK-B288-6.3.upd2.7z && 7z x -aoa -y SDK-B288-6.3.upd2.7z
+wget -O SDK-B288-6.7.7z https://github.com/c3pio-man/SDK_6.3.0/releases/download/6.7/SDK-B288-6.7.7z && 7z x -aoa -y SDK-B288-6.7.7z
